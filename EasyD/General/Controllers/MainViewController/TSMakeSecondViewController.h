3db401cd418937a600d5b2fbcbf5a1fc9e119855
Aries <jinying0218@163.com>
2014-10-07 12:01:19 +0800

    no message

---
+++ EasyD/General/Controllers/MainViewController/TSMakeSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSMakeSecondViewController.h
+//  EasyD
+//
+//  Created by Aries on 14-10-7.
+//  Copyright (c) 2014年 TechSailor. All rights reserved.
+//
+
+#import "TSBaseViewController.h"
+
+@interface TSMakeSecondViewController : TSBaseViewController
+
+@end
